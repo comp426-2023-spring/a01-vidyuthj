ab41bc9cad99db9e1b37f641e083b7f8390305c0
Vidyuth Jagadeesan <vidyuthj@email.unc.edu>
2023-01-22 13:51:43 -0500

    Fixed Error Version

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Require http module
 var http = require('http');
+
 // Require fs module
 var fs = require('fs');
 // Require minimist module (make sure you install this one via npm).
@@ -13,7 +14,7 @@ const port = argv.port || 3000;
 // Use the documentation for the Node.js `fs` module. 
 // The function must read a file located at `./public/index.html` and do some stuff with it.
 // The stuff that should be inside this function is all below.
-fs.readFile('./public/index.html', 'utf-8', (err, data) => {
+fs.readFile(`./public/index.html`, 'utf-8', (err, data) => {
 
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
@@ -21,7 +22,7 @@ fs.readFile('./public/index.html', 'utf-8', (err, data) => {
         console.error(err);
         return;
     }
-}
+
 
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
@@ -39,8 +40,7 @@ fs.readFile('./public/index.html', 'utf-8', (err, data) => {
 // Put the exact message `Server listening on port ${port}` on the console log. 
 server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
-})
-
+    })
 });
 
 
